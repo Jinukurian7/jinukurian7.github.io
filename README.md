26825502c3012eca71ad07841c7dc61e80b2973e
jinukurian_7 <jinu@keyvalue.systems>
2022-12-01 17:54:25 +0530

    made age, experience dynamicaly

--- README.md
+++ README.md
@@ -3,21 +3,21 @@
 Source code for my personal website. MIT Licensed. :octocat:
 
 Built using:
-* PUG (HTML5 template engine)
-* SASS
-* jQuery
-* GSAP (for animation)
-* GitHub Pages for hosting
 
+- PUG (HTML5 template engine)
+- SASS
+- jQuery
+- GSAP (for animation)
+- GitHub Pages for hosting
 
 To start, (assuming you have github page)
 
 1. Clone the repo
-2. Open terminal and enter to this directory
-3. Run npm install
-4. Run gulp (will opening your browser at http://localhost:3000)
-5. Run npm run deploy to publish the page. 
+2. Open terminal and enter this directory
+3. Run 'npm install'
+4. Run 'gulp' (will open your browser at http://localhost:3000)
+5. Run 'npm run deploy' to publish the page.
 
 Happy coding!
 
-Feel free to contact me if you have any doubt
+Feel free to contact me at jinukurian7@gmail.com if you have any doubt
--- src/lib/scripts.js
+++ src/lib/scripts.js
@@ -1,159 +1,166 @@
 (function ($) {
-    'use strict';
-    const JK = {
-        init: function() {
-            this.onLoad();
-            this.winLoad();
-            this.resizeListner();
-            this.scrollListner();
-            this.navToggle();
-            this.preloader();
-            this.loadAnim();
-            this.skillProgress();
-            this.timeline();
-        },
-        settings: {
-            windowWidth: $(window).width(),
-            windowheight: $(window).height(),
-            scrollClassTrigger: $(".hero-section").height() - 250
-        },
-        onLoad: () => {
-            $(document).ready(() => {
-                
-            });
-        },
+  "use strict";
+  const JK = {
+    init: function () {
+      this.onLoad();
+      this.winLoad();
+      this.resizeListner();
+      this.scrollListner();
+      this.navToggle();
+      this.preloader();
+      this.loadAnim();
+      this.skillProgress();
+      this.timeline();
+    },
+    settings: {
+      windowWidth: $(window).width(),
+      windowheight: $(window).height(),
+      scrollClassTrigger: $(".hero-section").height() - 250,
+    },
+    onLoad: () => {
+      $(document).ready(() => {});
+    },
 
-        winLoad: () => {
-            $(window).on('load', () => {
-                // $('body').addClass('page-loaded');
-            });
-        },
+    winLoad: () => {
+      $(window).on("load", () => {
+        // $('body').addClass('page-loaded');
+      });
+    },
 
-        resizeListner: () => {
-            $(window).on("load resize", () => {
-                JK.settings.windowWidth = $(window).width();
-            });
-        },
-       
-        // ScrollListner
-        scrollListner: () => {
-            $(window).on('load scroll', function () {
-                if ($(window).scrollTop() > JK.settings.scrollClassTrigger) {
-                    $('header').addClass('fixed');
-                } else {
-                    $('header').removeClass('fixed');
-                }
-            });
+    resizeListner: () => {
+      $(window).on("load resize", () => {
+        JK.settings.windowWidth = $(window).width();
+      });
+    },
 
-            $(window).on('mousewheel DOMMouseScroll', (event) => {
-                var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-                if (wd < 0) {
-                    $('body').removeClass('scrollingUp');
-                    $('body').addClass('scrollingDown');
-                } else {
-                    $('body').removeClass('scrollingDown');
-                    $('body').addClass('scrollingUp')
-                }
-            });
-        },
-
-        navToggle: () => {
-            $(".navbar-toggler").click(function() {
-                $("body, .navbar").toggleClass('menu-open');
-            })
-        },
-
-        // Preloader
-        preloader: function() {
-            let preloader = $(".preloader");
-            let cover = $(".preloader-cover");
-            let preloaderContent = $(".preloader-content > *");
-            let progressBar = $(".progressbar");
-            let count = $(".progressbar-text");
-            let tl = gsap.timeline();
-            tl.from(preloaderContent, 0.5,{
-                y: -10,
-                autoAlpha: 0, 
-                stagger: 0.5
-            })
-            .from(progressBar, 2, {
-                delay: 0.5,
-                strokeDashoffset: 100,
-                ease:Linear.easeNone,
-                onUpdate: function() {
-                    let percentage = Math.round(this.progress() * 100)
-                    count.text(percentage + "%")
-                }
-            })
-            .to(preloaderContent, 0.2,{
-                y: -10,
-                autoAlpha: 0, 
-            }, "+=0.5")
-            .to(cover, 1, {
-                delay: 0.25,
-                x: "-100%",
-                ease: Power4.easeOut
-            })
-            .to(preloader, 1, {
-                x: "-100%",
-                ease: Power4.easeOut
-            }, "-=0.25")
-            .to(preloader, 0.5, {
-                autoAlpha: 0
-            })
-            .add(function() {
-                $("body").addClass("page-loaded");
-            }, "-=1");
-        },
-
-        loadAnim: () => {
-
-            gsap.from(".profile-list li", {
-                autoAlpha: 0,
-                y: 10,
-                ease: Power4.easeOut,
-                delay: 0.5,
-                stagger: 0.25,
-                scrollTrigger: {
-                    trigger: ".profile-list"
-                }
-           })
-           gsap.from(".social li", {
-                autoAlpha: 0,
-                y: 10,
-                ease: Power4.easeOut,
-                delay: 0.5,
-                stagger: 0.25,
-                scrollTrigger: {
-                    trigger: ".social"
-                }
-           })
-        },
-
-        skillProgress: () => {
-            gsap.to(".bar-fill", 1.5, { 
-                width: function(index, target) {
-                    return target.dataset.value
-                }, 
-                stagger: 0.25,
-                ease: "power3.out",
-                scrollTrigger: {
-                    trigger: ".skill-box"
-                }
-            });
-        },
-
-        timeline: () => {
-            // gsap.to('.timeline-box')
-            gsap.utils.toArray(".timeline-box").forEach(function(section) {
-                gsap.from(section, 1.5, {
-                    ease: "power3.out",
-                    opacity:0,
-                    y: 100,
-                    scrollTrigger: section
-                });
-            });
+    // ScrollListner
+    scrollListner: () => {
+      $(window).on("load scroll", function () {
+        if ($(window).scrollTop() > JK.settings.scrollClassTrigger) {
+          $("header").addClass("fixed");
+        } else {
+          $("header").removeClass("fixed");
         }
-    };
-    JK.init();
-}(jQuery));
+      });
+
+      $(window).on("mousewheel DOMMouseScroll", (event) => {
+        var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+        if (wd < 0) {
+          $("body").removeClass("scrollingUp");
+          $("body").addClass("scrollingDown");
+        } else {
+          $("body").removeClass("scrollingDown");
+          $("body").addClass("scrollingUp");
+        }
+      });
+    },
+
+    navToggle: () => {
+      $(".navbar-toggler").click(function () {
+        $("body, .navbar").toggleClass("menu-open");
+      });
+    },
+
+    // Preloader
+    preloader: function () {
+      let preloader = $(".preloader");
+      let cover = $(".preloader-cover");
+      let preloaderContent = $(".preloader-content > *");
+      let progressBar = $(".progressbar");
+      let count = $(".progressbar-text");
+      let tl = gsap.timeline();
+      tl.from(preloaderContent, 0.5, {
+        y: -10,
+        autoAlpha: 0,
+        stagger: 0.5,
+      })
+        .from(progressBar, 2, {
+          delay: 0.5,
+          strokeDashoffset: 100,
+          ease: Linear.easeNone,
+          onUpdate: function () {
+            let percentage = Math.round(this.progress() * 100);
+            count.text(percentage + "%");
+          },
+        })
+        .to(
+          preloaderContent,
+          0.2,
+          {
+            y: -10,
+            autoAlpha: 0,
+          },
+          "+=0.5"
+        )
+        .to(cover, 1, {
+          delay: 0.25,
+          x: "-100%",
+          ease: Power4.easeOut,
+        })
+        .to(
+          preloader,
+          1,
+          {
+            x: "-100%",
+            ease: Power4.easeOut,
+          },
+          "-=0.25"
+        )
+        .to(preloader, 0.5, {
+          autoAlpha: 0,
+        })
+        .add(function () {
+          $("body").addClass("page-loaded");
+        }, "-=1");
+    },
+
+    loadAnim: () => {
+      gsap.from(".profile-list li", {
+        autoAlpha: 0,
+        y: 10,
+        ease: Power4.easeOut,
+        delay: 0.5,
+        stagger: 0.25,
+        scrollTrigger: {
+          trigger: ".profile-list",
+        },
+      });
+      gsap.from(".social li", {
+        autoAlpha: 0,
+        y: 10,
+        ease: Power4.easeOut,
+        delay: 0.5,
+        stagger: 0.25,
+        scrollTrigger: {
+          trigger: ".social",
+        },
+      });
+    },
+
+    skillProgress: () => {
+      gsap.to(".bar-fill", 1.5, {
+        width: function (index, target) {
+          return target.dataset.value;
+        },
+        stagger: 0.25,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".skill-box",
+        },
+      });
+    },
+
+    timeline: () => {
+      // gsap.to('.timeline-box')
+      gsap.utils.toArray(".timeline-box").forEach(function (section) {
+        gsap.from(section, 1.5, {
+          ease: "power3.out",
+          opacity: 0,
+          y: 100,
+          scrollTrigger: section,
+        });
+      });
+    },
+  };
+  JK.init();
+})(jQuery);
